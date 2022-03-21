32ea7c20a19e770add0d06621b740bfbcfb388db
victor.druc <druc92@gmail.com>
2022-03-21 20:59:56 +0200

    add-indexed-db

--- front/public/sw-analytics.js
+++ front/public/sw-analytics.js
@@ -1,32 +1,45 @@
-self.addEventListener("install", e=>{
-    console.info("analytics: SW installed")
-})
+function DbIndex(dbName, dbColl) {
+  return {
+    upgradeneeded() {
+      const dbConn = indexedDB.open(dbName);
+      dbConn.onupgradeneeded = (dbEvent) => {
+        console.info("DB opened successfully");
+        const db = dbEvent.target.result;
+        db.createObjectStore(dbColl);
+      };
+    },
+    success(data) {
+      const dbConn = indexedDB.open(dbName);
+      dbConn.onsuccess = (dbEvent) => {
+        const db = dbEvent.target.result;
+        db.transaction(dbColl, "readwrite")
+          .objectStore(dbColl)
+          .add(data, Date.now());
+      };
+    },
+  };
+}
 
-self.addEventListener("activate", e=>{
-    console.info("analytics: SW activated")
-    const dbConn = indexedDB.open("analytics")
-    dbConn.onupgradeneeded = dbEvent => {
-        console.info("DB opened successfully")
-        const db = dbEvent.target.result
-        db.createObjectStore("actions")
-    }
-})
+const dbIndex = DbIndex("analytics", "actions");
 
-self.addEventListener("fetch", e=>{
-    if(e.request.url.endsWith("/api/analytics")) {
-        if(!navigator.onLine) {
-            e.request.json().then(data=>{
-                console.info("SAVING offline data", data)
-                const dbConn = indexedDB.open("analytics")
-                dbConn.onsuccess = dbEvent=>{
-                    const db = dbEvent.target.result
-                    db.transaction("actions","readwrite")
-                        .objectStore("actions")
-                        .add(data, Date.now())
-                }
-            })
-        } else {
-            console.info("SENDING online data")
-        }
+self.addEventListener("install", (e) => {
+  console.info("analytics: SW installed");
+});
+
+self.addEventListener("activate", (e) => {
+  console.info("analytics: SW activated");
+  dbIndex.upgradeneeded();
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.url.endsWith("/api/analytics")) {
+    if (!navigator.onLine) {
+      e.request.json().then((data) => {
+        console.info("SAVING offline data", data);
+        dbIndex.success(data);
+      });
+    } else {
+      console.info("SENDING online data");
     }
-})
+  }
+});
